eca18921de6fc42683a2e477aa9e115e390f65c2
Armin Broubakarian <armin.broubakarian@gmail.com>
2024-11-10 18:46:50 -0600

    feat: add husky and commitlint

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,14 @@
-"use strict";
-console.log('hello world');
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+
+console.log('hello world!');
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
